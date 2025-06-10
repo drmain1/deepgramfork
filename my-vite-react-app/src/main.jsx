cb21d0c20611f510dd5cbe54a06958baf7b884c3
drmain1 <dtmain@gmail.com>
2025-06-09 17:23:16 -0700

    auth page little better

--- my-vite-react-app/src/main.jsx
+++ my-vite-react-app/src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './styles.css';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import CustomAuthenticator from './components/CustomAuthenticator.jsx';
 import { Amplify } from 'aws-amplify';
 import { amplifyConfig } from './amplifyconfigure.js';
 import { AuthProvider } from './contexts/AuthContext.jsx';
@@ -15,14 +14,14 @@ Amplify.configure(amplifyConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Authenticator signUpAttributes={['email']}>
-      {({ signOut, user }) => (
+    <CustomAuthenticator>
+      {() => (
         <AuthProvider>
           <UserSettingsProvider>
             <App />
           </UserSettingsProvider>
         </AuthProvider>
       )}
-    </Authenticator>
+    </CustomAuthenticator>
   </React.StrictMode>
 );
